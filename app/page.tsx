89ce53c624c8108773e145f3c7d86e5755dd314b
joetechgeek <joetechgeek@gmail.com>
2024-10-18 19:22:57 +0300

    supabase config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import { supabase } from '../utils/supabase'
 
-export default function Home() {
+export default async function Home() {
+  const { data, error } = await supabase.from('test').select('message').single()
+
+  if (error) {
+    console.error('Error fetching data:', error)
+    return <div>Error loading data</div>
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost', process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '')],
+  },
+};
 
 export default nextConfig;
